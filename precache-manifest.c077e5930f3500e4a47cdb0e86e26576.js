a4efd64041db386f82a081d24bcbf6116a6c503b
Flávio Ricardo <flavioricardo@live.com>
2020-05-31 02:32:19 -0300

    Updates

---
+++ precache-manifest.c077e5930f3500e4a47cdb0e86e26576.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39976f5c454505ab5c41ec8a62ab65a3",
+    "url": "/agendaesports/index.html"
+  },
+  {
+    "revision": "61a3302c3afc62935992",
+    "url": "/agendaesports/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6879dc56aa1934dc3735",
+    "url": "/agendaesports/static/js/2.02687daf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/agendaesports/static/js/2.02687daf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a3302c3afc62935992",
+    "url": "/agendaesports/static/js/main.26f7f87a.chunk.js"
+  },
+  {
+    "revision": "ffe1320445ce1caab00b",
+    "url": "/agendaesports/static/js/runtime-main.8ea34a22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/agendaesports/static/media/logo.5d5d9eef.svg"
+  }
+]);
